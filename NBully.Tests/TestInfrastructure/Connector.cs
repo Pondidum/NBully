50f2fd527fa7e8e7aee6418538389a9b8ec44b60
andy <andy@andydote.co.uk>
2016-08-14 13:13:59 +0100

    test: add connector to allow sending simple messages to a node

---
+++ NBully.Tests/TestInfrastructure/Connector.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NSubstitute;
+
+namespace NBully.Tests.TestInfrastructure
+{
+	public class Connector
+	{
+		private readonly IBullyCommunicator _connector;
+		private Action<int> _sendStartElection;
+		private Action<int> _sendAlive;
+		private Action<int> _sendWin;
+
+		public Connector()
+		{
+			_connector = Substitute.For<IBullyCommunicator>();
+
+			_connector
+				.When(c => c.OnReceivedStartElection(Arg.Any<Action<int>>()))
+				.Do(ci => _sendStartElection = ci.Arg<Action<int>>());
+
+			_connector
+				.When(c => c.OnReceivedAlive(Arg.Any<Action<int>>()))
+				.Do(ci => _sendAlive = ci.Arg<Action<int>>());
+
+			_connector
+				.When(c => c.OnReceivedWin(Arg.Any<Action<int>>()))
+				.Do(ci => _sendWin = ci.Arg<Action<int>>());
+		}
+
+		public IBullyCommunicator Communicator => _connector;
+
+		public void SendStartElection(int fromPid)
+		{
+			_sendStartElection(fromPid);
+		}
+
+		public void SendAlive(int fromPid)
+		{
+			_sendAlive(fromPid);
+		}
+
+		public void SendWin(int fromPid)
+		{
+			_sendWin(fromPid);
+		}
+	}
+}
