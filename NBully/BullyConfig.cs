360e4708ecb0a6ffb87cab7f93a72c5fe33d347c
andy <andy@andydote.co.uk>
2016-08-14 13:15:00 +0100

    communicator: move into separate file

--- NBully/BullyConfig.cs
+++ NBully/BullyConfig.cs
@@ -18,20 +18,6 @@ namespace NBully
 		}
 	}
 
-	public interface IBullyCommunicator
-	{
-		int OwnerProcessID { set; }
-
-		void StartElection();
-		void SendAlive(int toProcessID);
-		void BroadcastWin();
-
-		void OnReceivedStartElection(Action<int> handler);
-		void OnReceivedAlive(Action<int> handler);
-		void OnReceivedWin(Action<int> handler);
-
-	}
-
 	public class BullyNode
 	{
 		//private readonly BullyConfig _config;
---
+++ NBully/IBullyCommunicator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NBully
+{
+	public interface IBullyCommunicator
+	{
+		int OwnerProcessID { set; }
+
+		void StartElection();
+		void SendAlive(int toProcessID);
+		void BroadcastWin();
+
+		void OnReceivedStartElection(Action<int> handler);
+		void OnReceivedAlive(Action<int> handler);
+		void OnReceivedWin(Action<int> handler);
+
+	}
+}
